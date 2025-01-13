89b7c559f9bd24cd7e6b777e7069d2b11f2cf30f
Samuel Neugber <samuel.neugber@gmail.com>
2025-01-13 21:21:02 +0100

    Install ruff

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import time
 from loguru import logger
 
-logger.add('/var/log/my_python_service.log', level="INFO")
+logger.add("/var/log/my_python_service.log", level="INFO")
+
 
 def main():
     while True:
         logger.info("Service is running...")
         time.sleep(60)
+
 
 if __name__ == "__main__":
     main()
